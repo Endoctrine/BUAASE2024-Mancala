3bd8dca604fc8edb9c50c3a5fdd0ae03eb640801
王继来 <21373215@buaa.edu.cn>
2024-04-04 14:25:51 +0800

    [feat]: Finish bocchi_shut_up() and pass basic tests.

--- T1/t1_rust/src/lib.rs
+++ T1/t1_rust/src/lib.rs
@@ -1,8 +1,27 @@
+use std::collections::HashMap;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 pub fn bocchi_shut_up(flag: i32, seq: &[i32], size: i32) -> i32 {
-    10
+    let frequency = seq.iter()
+        .filter(|&x| {
+            *x / 10 == flag
+        })
+        .fold(HashMap::new(), |mut acc, x| {
+            *acc.entry(*x).or_insert(0) += 1;
+            acc
+        });
+
+    let max_freq = frequency.values().max().copied().unwrap_or(0);
+    let most_freq_num = frequency.iter().filter(|&(_, freq)| {
+        *freq == max_freq
+    }).map(|(num, _)| *num).collect::<Vec<_>>();
+
+    if most_freq_num.len() > 1 {
+        10
+    } else {
+        most_freq_num[0]
+    }
 }
 
 #[cfg(test)]
@@ -11,11 +30,11 @@ mod tests {
 
     #[test]
     fn bocchi_shut_up_test_when_10() {
-        assert_eq!(bocchi_shut_up(1, &[11,12,13], 3), 10);
+        assert_eq!(bocchi_shut_up(1, &[11, 12, 13], 3), 10);
     }
 
     #[test]
     fn bocchi_shut_up_test_when_not_10() {
-        assert_eq!(bocchi_shut_up(1, &[11,12,12], 3), 12);
+        assert_eq!(bocchi_shut_up(1, &[11, 12, 12], 3), 12);
     }
 }
